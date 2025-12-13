8bc78c371aa188dc8b406434d8c254529da02ac5
Zhuo Zhang <imzhuo@foxmail.com>
2025-12-13 12:28:02 +0800

    Add demo6 with basic Win32 structure definition example

---
+++ demo6/main.cpp
@@ -0,0 +1,14 @@
+#include "windef.h"
+
+// Win32 style structure definition example
+//
+// typedef struct tagPOINT
+// {
+//     LONG  x;
+//     LONG  y;
+// } POINT, *PPOINT, NEAR *NPPOINT, FAR *LPPOINT;
+
+int main()
+{
+    
+}
