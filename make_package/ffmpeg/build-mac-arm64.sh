851d1645486f6073e20db7853415cc5448563b34
Zhuo Zhang <imzhuo@foxmail.com>
2025-12-21 00:07:21 +0800

    build ffmpeg and opencv + ffmpeg on macOS

---
+++ make_package/ffmpeg/build-mac-arm64.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source checkout_ffmpeg_n7.0.3.sh
+
+set -x
+
+build_dir="$work_dir/build-mac-arm64"
+
+# Clean build
+# [ -d "$build_dir" ] && rm -rf "$build_dir"
+
+: "${ZZPKG_ROOT:=$HOME/.zzpkg}"
+
+mkdir -p "$build_dir"
+pushd "$build_dir"
+../ffmpeg/configure \
+  --prefix="$ZZPKG_ROOT/$pkg_name/$version/mac-arm64" \
+  --disable-everything \
+  --enable-decoder=h264,aac \
+  --enable-parser=h264,aac \
+  --enable-demuxer=mp4,mov \
+  --enable-protocol=file \
+  --disable-network \
+  --disable-hwaccels \
+  --disable-doc \
+  --disable-shared \
+  --enable-static \
+  --enable-pic
+make
+make install
+popd
---
+++ make_package/ffmpeg/checkout_ffmpeg_n7.0.3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+# 如果 SKIP_CLONE 被设为非空值，则跳过 checkout_repo 调用
+: "${SKIP_CLONE:=}"
+
+source ../checkout_repo.sh
+
+# === Main ===
+work_dir=$(pwd)
+tag="n7.0.3"
+version="7.0.3"
+pkg_name="ffmpeg"
+source_dir="$work_dir/ffmpeg"
+
+if [[ -z "$SKIP_CLONE" ]]; then
+    checkout_repo "ffmpeg" \
+            "https://git.ffmpeg.org/ffmpeg.git" \
+            "https://gitee.com/mirrors/ffmpeg.git" \
+            "$work_dir/ffmpeg" \
+            "$tag"
+fi
