7db27aeb615cd7f55766fd21197542c0ebdc0569
Zhuo Zhang <imzhuo@foxmail.com>
2025-12-20 19:33:30 +0800

    refactor zzpkg_find() which use default path when given package name and version

--- cmake/zzpkg.cmake
+++ cmake/zzpkg.cmake
@@ -16,9 +16,38 @@ if(NOT CMAKE_INSTALL_RPATH)
   set(CMAKE_INSTALL_RPATH "$ORIGIN:$ORIGIN/../lib")
 endif()
 
+macro(zzpkg_detect_platform)
+  # determine platform
+  if(ANDROID)
+    set(ZZPKG_PLATFORM "android")
+  elseif(WIN32)
+    set(ZZPKG_PLATFORM "windows")
+  elseif(APPLE)
+    set(ZZPKG_PLATFORM "mac")
+  elseif(CMAKE_SYSTEM_NAME STREQUAL "linux")
+    set(ZZPKG_PLATFORM "linux")
+  else()
+    message(ERROR "unknown platform: ${CMAKE_SYSTEM_NAME}")
+  endif()
+endmacro()
+
+macro(zzpkg_detect_arch)
+  # determine arch
+  if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" OR CMAKE_SYSTEM_PROCESSOR STREQUAL "AMD64")
+    set(ZZPKG_ARCH "x64")
+  elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64" OR CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64")
+    set(ZZPKG_ARCH "arm64")
+  else()
+    message(ERROR "unknown architecture: ${CMAKE_SYSTEM_PROCESSOR}")
+  endif()
+endmacro()
+
+zzpkg_detect_platform()
+zzpkg_detect_arch()
+
 # Macro to find a package given platform-specific root paths
 # Usage:
-# zzpkg_find([PACKAGE_NAME]               # Package name, required
+# zzpkg_find([PACKAGE_NAME/PACKAGE_VERSION] # Package name and version, required
 #   WINDOWS  "path/to/windows/package"    # Package install path on Windows, optional
 #   LINUX    "path/to/linux/package"      # Package install path on Linux, optional
 #   ANDROID  "path/to/android/package"    # Package install path on Android, optional
@@ -26,33 +55,45 @@ endif()
 #   COMPONENTS comp1 comp2 ...            # Required components, optional
 #   OPTIONAL_COMPONENTS optcomp1 optcomp2 ... # Optional components, optional
 # )
-macro(zzpkg_find PACKAGE_NAME)
+macro(zzpkg_find PACKAGE_NAME_AND_VERSION)
+  # 解析 PACKAGE_NAME_AND_VERSION 为 pkg_name 和 pkg_version
+  string(REPLACE "/" ";" _pkg_parts "${PACKAGE_NAME_AND_VERSION}")
+  list(LENGTH _pkg_parts _parts_len)
+  list(GET _pkg_parts 0 PACKAGE_NAME)
+  if(_parts_len GREATER 1)
+    list(GET _pkg_parts 1 PACKAGE_VERSION)
+  else()
+    message(FATAL_ERROR "PACKAGE_NAME_AND_VERSION must be in the format 'PackageName/Version'")
+  endif()
+
   cmake_parse_arguments(
     ARG
     ""
-    "WINDOWS;LINUX;ANDROID;MACOS"
+    "WINDOWS;LINUX;ANDROID;MAC;DEFAULT"
     "COMPONENTS;OPTIONAL_COMPONENTS"
     ${ARGN}
   )
 
-  # 根据平台选择路径
-  if(ANDROID AND ARG_ANDROID)
+  # Default path construction
+  set(package_root "${ZZPKG_ROOT}/${PACKAGE_NAME}/${PACKAGE_VERSION}/${ZZPKG_PLATFORM}-${ZZPKG_ARCH}")
+
+  if((ZZPKG_PLATFORM STREQUAL "android") AND ARG_ANDROID)
     set(package_root "${ARG_ANDROID}")
-    set(platform_name "Android")
-  elseif(WIN32 AND ARG_WINDOWS)
+  elseif((ZZPKG_PLATFORM STREQUAL "windows") AND ARG_WINDOWS)
     set(package_root "${ARG_WINDOWS}")
-    set(platform_name "Windows")
-  elseif(APPLE AND ARG_MACOS)
-    set(package_root "${ARG_MACOS}")
-    set(platform_name "macOS")
-  elseif(UNIX AND NOT ANDROID AND ARG_LINUX)
+  elseif((ZZPKG_PLATFORM STREQUAL "mac") AND ARG_MAC)
+    set(package_root "${ARG_MAC}")
+  elseif((ZZPKG_PLATFORM STREQUAL "linux") AND ARG_LINUX)
     set(package_root "${ARG_LINUX}")
-    set(platform_name "Linux")
-  else()
-    message(FATAL_ERROR "No ${PACKAGE_NAME} path provided for current platform")
+  elseif(ARG_DEFAULT)
+    set(package_root "${ARG_DEFAULT}")
   endif()
 
-  message(STATUS "Searching ${PACKAGE_NAME} on ${platform_name}")
+  if(NOT package_root)
+    message(FATAL_ERROR "No ${PACKAGE_NAME_AND_VERSION} path provided for current platform")
+  endif()
+
+  message(STATUS "Searching ${PACKAGE_NAME_AND_VERSION} on ${ZZPKG_PLATFORM}")
   message(STATUS "Root directory: ${package_root}")
 
   # 常见的 CMake config 文件位置
@@ -78,12 +119,10 @@ macro(zzpkg_find PACKAGE_NAME)
   set(config_found FALSE)
   foreach(subdir ${candidate_subdirs})
     set(config_path "${package_root}/${subdir}")
-    
     set(config_files
       "${config_path}/${PACKAGE_NAME}Config.cmake"
       "${config_path}/${PACKAGE_NAME}-config.cmake"
     )
-    
     foreach(config_file ${config_files})
       if(EXISTS "${config_file}")
         set(${PACKAGE_NAME}_DIR "${config_path}")
@@ -92,7 +131,6 @@ macro(zzpkg_find PACKAGE_NAME)
         break()
       endif()
     endforeach()
-    
     if(config_found)
       break()
     endif()
@@ -113,7 +151,7 @@ macro(zzpkg_find PACKAGE_NAME)
 
   message(STATUS "${PACKAGE_NAME}_FOUND: ${${PACKAGE_NAME}_FOUND}")
   message(STATUS "${PACKAGE_NAME}_VERSION: ${${PACKAGE_NAME}_VERSION}")
-  
+
   # 清理临时变量
   unset(ARG_WINDOWS)
   unset(ARG_LINUX)
@@ -129,5 +167,7 @@ macro(zzpkg_find PACKAGE_NAME)
   unset(config_path)
   unset(config_files)
   unset(config_file)
-  
+  unset(_pkg_parts)
+  unset(PACKAGE_NAME)
+  unset(PACKAGE_VERSION)
 endmacro()
