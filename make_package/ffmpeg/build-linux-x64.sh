22d4d825599734647c45b13fd0a5d503194533f0
Zhuo Zhang <imzhuo@foxmail.com>
2025-12-21 09:39:46 +0800

    build OpenCV with FFmpeg7 and GTK2 for reading .mp4 and show it

---
+++ make_package/ffmpeg/build-linux-x64.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source checkout_ffmpeg_n7.0.3.sh
+
+set -x
+
+# sudo apt-get install -y nasm
+
+build_dir="$work_dir/build-linux-x64"
+
+# Clean build
+[ -d "$build_dir" ] && rm -rf "$build_dir"
+
+: "${ZZPKG_ROOT:=$HOME/.zzpkg}"
+
+mkdir -p "$build_dir"
+pushd "$build_dir"
+../ffmpeg/configure \
+  --prefix="$ZZPKG_ROOT/$pkg_name/$version/linux-x64" \
+  --disable-everything \
+  --enable-decoder=h264,aac \
+  --enable-parser=h264,aac \
+  --enable-demuxer=mp4,mov \
+  --enable-protocol=file \
+  --disable-network \
+  --disable-hwaccels \
+  --disable-doc \
+  --disable-shared \
+  --enable-static \
+  --enable-pic \
+  --disable-x86asm \
+  --disable-inline-asm
+make
+make install
+popd
--- make_package/opencv/build-linux-x64.sh
+++ make_package/opencv/build-linux-x64.sh
@@ -3,21 +3,45 @@
 source checkout_ocv_4.12.0.sh
 
 set -x
+# set -u  # 如果有这行，先注释掉或者正确初始化变量
+
+sudo apt install -y libgtk2.0-dev
 
 build_dir="$work_dir/build-linux-x64"
 
+: "${ZZPKG_ROOT:=$HOME/.zzpkg}"
+
 # Clean build
-# [ -d "$build_dir" ] && rm -rf "$build_dir"
+[ -d "$build_dir" ] && rm -rf "$build_dir"
+
+FFMPEG_PREFIX="$ZZPKG_ROOT/ffmpeg/7.0.3/linux-x64"
+
+# 初始化 PKG_CONFIG_PATH（防止 set -u 报错）
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-}${PKG_CONFIG_PATH:+:}$FFMPEG_PREFIX/lib/pkgconfig"
 
 CMAKE_GENERATOR_OPTIONS=(
-      -G"Ninja"
+    -G"Ninja"
+    -DWITH_FFMPEG=ON
+    -DVIDEOIO_ENABLE_PLUGINS=OFF
+    
+    # 方法1: 通过 CMAKE_PREFIX_PATH 让 CMake 自动查找
+    -DCMAKE_PREFIX_PATH="$FFMPEG_PREFIX"
+    
+    # 方法2: 手动指定每个库
+    -DFFMPEG_INCLUDE_DIR="$FFMPEG_PREFIX/include"
+    -DFFMPEG_LIBAVCODEC="$FFMPEG_PREFIX/lib/libavcodec.a"
+    -DFFMPEG_LIBAVFORMAT="$FFMPEG_PREFIX/lib/libavformat.a"
+    -DFFMPEG_LIBAVUTIL="$FFMPEG_PREFIX/lib/libavutil.a"
+    -DFFMPEG_LIBSWSCALE="$FFMPEG_PREFIX/lib/libswscale.a"
+    -DFFMPEG_LIBSWRESAMPLE="$FFMPEG_PREFIX/lib/libswresample.a"
 )
 
-: "${ZZPKG_ROOT:=$HOME/.zzpkg}"
 install_dir="$ZZPKG_ROOT/$pkg_name/$version/linux-x64"
+
 cmake "${CMAKE_GENERATOR_OPTIONS[@]}" "${CMAKE_OPTIONS[@]}" \
       -DCMAKE_INSTALL_PREFIX="$install_dir" \
       -DCMAKE_BUILD_TYPE=Release \
       -S "$source_dir" -B "$build_dir" -Wno-deprecated
+
 cmake --build "$build_dir" --config Release
 cmake --build "$build_dir" --target install --config Release
