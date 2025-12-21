f884e33276e6cdb2e2e9e8f81baa74b860f65c8c
Zhuo Zhang <imzhuo@foxmail.com>
2025-12-21 14:52:30 +0800

    Build glfw 3.4 on windows

---
+++ make_package/glfw/checkout_glfw_3.4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+
+# 如果 SKIP_CLONE 被设为非空值，则跳过 checkout_repo 调用
+: "${SKIP_CLONE:=}"
+
+source ../checkout_repo.sh
+
+# === Main ===
+work_dir=$(pwd)
+tag="3.4"
+version="$tag"
+pkg_name="glfw3"
+source_dir="$work_dir/glfw"
+
+if [[ -z "$SKIP_CLONE" ]]; then
+    checkout_repo "glfw" \
+            "https://github.com/glfw/glfw.git" \
+            "https://gitee.com/mirrors/glfw.git" \
+            "$work_dir/glfw" \
+            "$tag"
+fi
+
+CMAKE_OPTIONS=(
+    -DCMAKE_DEBUG_POSTFIX="_d"
+)
