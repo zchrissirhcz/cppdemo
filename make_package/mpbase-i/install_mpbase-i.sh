c522e657016fe50430baeeb5fb834c4d3bbcfb48
Zhuo Zhang <imzhuo@foxmail.com>
2025-12-25 20:30:05 +0800

    make package: mpbase-i/0.1

---
+++ make_package/mpbase-i/install_mpbase-i.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+
+: "${SKIP_CLONE:=}"
+
+source ../checkout_repo.sh
+
+# === Main ===
+work_dir=$(pwd)
+branch="master"
+version="$branch"
+pkg_name="mpbase-i"
+source_dir="$work_dir/ArcSoft_FreeSDK_Demo"
+
+if [[ -z "$SKIP_CLONE" ]]; then
+    checkout_repo "cereal" \
+        "https://github.com/smartkids77/ArcSoft_FreeSDK_Demo.git" \
+        "https://github.com/smartkids77/ArcSoft_FreeSDK_Demo.git" \
+        "$source_dir" \
+        "$branch"
+fi
+
+: "${ZZPKG_ROOT:=$HOME/.zzpkg}"
+
+# copy header files
+DST_DIR=${ZZPKG_ROOT}/mpbase-i/0.1/inc
+mkdir -p ${DST_DIR}
+cp $source_dir/ArcFace/QT/FRDemo/amcomdef.h $DST_DIR/
+cp $source_dir/ArcFace/QT/FRDemo/merror.h $DST_DIR/
+cp $source_dir/ArcFace/QT/FRDemo/asvloffscreen.h $DST_DIR/
