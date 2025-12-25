8938df11b727cfc919823891b5d23482d881f02a
Zhuo Zhang <imzhuo@foxmail.com>
2025-12-25 20:48:59 +0800

    make package: spdlog/1.16.0

---
+++ make_package/spdlog/install_spdlog_1.16.0.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+# 如果 SKIP_CLONE 被设为非空值，则跳过 checkout_repo 调用
+: "${SKIP_CLONE:=}"
+
+source ../checkout_repo.sh
+
+# === Main ===
+work_dir=$(pwd)
+tag="v1.16.0"
+version="1.16.0"
+pkg_name="spdlog"
+source_dir="$work_dir/spdlog"
+
+if [[ -z "$SKIP_CLONE" ]]; then
+    checkout_repo "spdlog" \
+            "https://github.com/gabime/spdlog.git" \
+            "https://gitee.com/mirrors_nigels-com/spdlog.git" \
+            "$source_dir" \
+            "$tag"
+fi
+
+: "${ZZPKG_ROOT:=$HOME/.zzpkg}"
+
+# copy header files
+mkdir -p ${ZZPKG_ROOT}/spdlog/1.16.0
+cp -R ${source_dir}/include ${ZZPKG_ROOT}/spdlog/1.16.0/inc
