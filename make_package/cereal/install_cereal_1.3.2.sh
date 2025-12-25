b5730047e4ec75b943b48aa31cf1a468e05337bd
Zhuo Zhang <imzhuo@foxmail.com>
2025-12-25 20:14:13 +0800

    make package: cereal/1.3.2

---
+++ make_package/cereal/install_cereal_1.3.2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+: "${SKIP_CLONE:=}"
+
+source ../checkout_repo.sh
+
+# === Main ===
+work_dir=$(pwd)
+tag="v1.3.2"
+version="1.3.2"
+pkg_name="cereal"
+source_dir="$work_dir/cereal"
+
+if [[ -z "$SKIP_CLONE" ]]; then
+    checkout_repo "cereal" \
+        "https://github.com/USCiLab/cereal.git" \
+        "https://gitee.com/mirrors/cereal.git" \
+        "$source_dir" \
+        "$tag"
+fi
+
+: "${ZZPKG_ROOT:=$HOME/.zzpkg}"
+
+# copy header files
+mkdir -p ${ZZPKG_ROOT}/cereal/1.3.2
+cp -R ${source_dir}/include ${ZZPKG_ROOT}/cereal/1.3.2/inc
