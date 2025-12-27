3fc78f1cc1ea3b4dbcc8fb6208f94e96a52559ce
Zhuo Zhang <imzhuo@foxmail.com>
2025-12-27 20:03:24 +0800

    support loading package with hint specified, e.g. OpenCV/4.12.0:static

--- cmake/zzpkg.cmake
+++ cmake/zzpkg.cmake
@@ -55,16 +55,25 @@ zzpkg_detect_arch()
 #   ANDROID  "path/to/android/package"    # Package install path on Android, optional
 #   MACOS    "path/to/macos/package"      # Package install path on macOS, optional
 # )
-macro(zzpkg_find PACKAGE_NAME_AND_VERSION)
-  # 解析 PACKAGE_NAME_AND_VERSION 为 pkg_name 和 pkg_version
-  string(REPLACE "/" ";" _pkg_parts "${PACKAGE_NAME_AND_VERSION}")
-  list(LENGTH _pkg_parts _parts_len)
-  list(GET _pkg_parts 0 PACKAGE_NAME)
-  if(_parts_len GREATER 1)
-    list(GET _pkg_parts 1 PACKAGE_VERSION)
-  else()
-    message(FATAL_ERROR "PACKAGE_NAME_AND_VERSION must be in the format 'PackageName/Version'")
+macro(zzpkg_find PACKAGE_RECIPE)
+  message(STATUS "Searching recipe ${PACKAGE_RECIPE} on platform ${ZZPKG_PLATFORM}")
+  # 解析 PACKAGE_RECIPE 为 pkg_name、pkg_version 和 pkg_hint (可选)
+  string(REPLACE "/" ";" _recipe_parts "${PACKAGE_RECIPE}")
+  string(REPLACE ":" ";" _recipe_parts "${_recipe_parts}")
+  list(LENGTH _recipe_parts _recipe_parts_len)  
+  if(_recipe_parts_len LESS 2)
+    message(FATAL_ERROR "PACKAGE_RECIPE must be in the format 'PackageName/Version[:hint]'")
   endif()
+  list(GET _recipe_parts 0 PACKAGE_NAME)
+  list(GET _recipe_parts 1 PACKAGE_VERSION)
+  set(PACKAGE_HINT)
+  if(_recipe_parts_len EQUAL 3)
+    list(GET _recipe_parts 2 PACKAGE_HINT)
+  endif()
+
+  message(STATUS "  PACKAGE_NAME: ${PACKAGE_NAME}")
+  message(STATUS "  PACKAGE_VERSION: ${PACKAGE_VERSION}")
+  message(STATUS "  PACKAGE_HINT: ${PACKAGE_HINT}")
 
   cmake_parse_arguments(
     ARG
@@ -74,41 +83,56 @@ macro(zzpkg_find PACKAGE_NAME_AND_VERSION)
     ${ARGN}
   )
 
+  if((ZZPKG_PLATFORM STREQUAL "android") AND ARG_ANDROID)
+    set(CUSTOM_PACKAGE_ROOT "${ARG_ANDROID}")
+  elseif((ZZPKG_PLATFORM STREQUAL "windows") AND ARG_WINDOWS)
+    set(CUSTOM_PACKAGE_ROOT "${ARG_WINDOWS}")
+  elseif((ZZPKG_PLATFORM STREQUAL "mac") AND ARG_MAC)
+    set(CUSTOM_PACKAGE_ROOT "${ARG_MAC}")
+  elseif((ZZPKG_PLATFORM STREQUAL "linux") AND ARG_LINUX)
+    set(CUSTOM_PACKAGE_ROOT "${ARG_LINUX}")
+  elseif(ARG_DEFAULT)
+    set(CUSTOM_PACKAGE_ROOT "${ARG_DEFAULT}")
+  else()
+    set(CUSTOM_PACKAGE_ROOT)
+  endif()
+
   # Default path construction
   # try 1: if exists `inc` directory under ZZPKG_ROOT/PACKAGE_NAME/PACKAGE_VERSION, treat it as header-only library
   set(PACKAGE_PLATFORM_INDEPENDENT_ROOT "${ZZPKG_ROOT}/${PACKAGE_NAME}/${PACKAGE_VERSION}")
   set(PACKAGE_PLATFORM_DEPENDENT_ROOT "${ZZPKG_ROOT}/${PACKAGE_NAME}/${PACKAGE_VERSION}/${ZZPKG_PLATFORM}-${ZZPKG_ARCH}")
-  if(EXISTS "${PACKAGE_PLATFORM_INDEPENDENT_ROOT}/inc")
-    set(PACKAGE_ROOT "${PACKAGE_PLATFORM_INDEPENDENT_ROOT}")
-    add_library(${PACKAGE_NAME} INTERFACE)
-    target_include_directories(${PACKAGE_NAME} INTERFACE "${PACKAGE_PLATFORM_INDEPENDENT_ROOT}/inc")
-    set_target_properties(${PACKAGE_NAME} PROPERTIES
-      VERSION ${PACKAGE_VERSION}
-    )
-    add_library(zzpkg::${PACKAGE_NAME} ALIAS ${PACKAGE_NAME})
-  elseif(EXISTS ${PACKAGE_PLATFORM_DEPENDENT_ROOT})
-    set(PACKAGE_ROOT "${PACKAGE_PLATFORM_DEPENDENT_ROOT}")
-    if((ZZPKG_PLATFORM STREQUAL "android") AND ARG_ANDROID)
-      set(package_root "${ARG_ANDROID}")
-    elseif((ZZPKG_PLATFORM STREQUAL "windows") AND ARG_WINDOWS)
-      set(package_root "${ARG_WINDOWS}")
-    elseif((ZZPKG_PLATFORM STREQUAL "mac") AND ARG_MAC)
-      set(package_root "${ARG_MAC}")
-    elseif((ZZPKG_PLATFORM STREQUAL "linux") AND ARG_LINUX)
-      set(package_root "${ARG_LINUX}")
-    elseif(ARG_DEFAULT)
-      set(package_root "${ARG_DEFAULT}")
-    else()
-      set(package_root "${PACKAGE_ROOT}")
+  set(PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT "${PACKAGE_PLATFORM_DEPENDENT_ROOT}")
+  if(PACKAGE_HINT)
+    STRING(APPEND PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT "-${PACKAGE_HINT}")
+  endif()
+
+  message(STATUS "  PACKAGE_PLATFORM_INDEPENDENT_ROOT: ${PACKAGE_PLATFORM_INDEPENDENT_ROOT}")
+  message(STATUS "  PACKAGE_PLATFORM_DEPENDENT_ROOT: ${PACKAGE_PLATFORM_DEPENDENT_ROOT}")
+  message(STATUS "  PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT: ${PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT}")
+
+  set(CANDIDATE_PACKAGE_ROOT_LIST)
+  if(CUSTOM_PACKAGE_ROOT)
+    list(APPEND CANDIDATE_PACKAGE_ROOT_LIST ${CUSTOM_PACKAGE_ROOT})
+  endif()
+  if(PACKAGE_HINT)
+    list(APPEND CANDIDATE_PACKAGE_ROOT_LIST ${PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT})
+  endif()
+  list(APPEND CANDIDATE_PACKAGE_ROOT_LIST ${PACKAGE_PLATFORM_INDEPENDENT_ROOT})
+
+  foreach(PACKAGE_ROOT ${CANDIDATE_PACKAGE_ROOT_LIST})
+    message(STATUS "  Try find package with PACKAGE_ROOT: ${PACKAGE_ROOT}")
+    if(EXISTS "${PACKAGE_ROOT}/inc")
+      add_library(${PACKAGE_NAME} INTERFACE)
+      target_include_directories(${PACKAGE_NAME} INTERFACE "${PACKAGE_PLATFORM_INDEPENDENT_ROOT}/inc")
+      set_target_properties(${PACKAGE_NAME} PROPERTIES
+        VERSION ${PACKAGE_VERSION}
+      )
+      add_library(zzpkg::${PACKAGE_NAME} ALIAS ${PACKAGE_NAME})
+      message(STATUS "  Created INTERFACE target: zzpkg::${PACKAGE_NAME}")
+      break()
     endif()
-
-    if(NOT package_root)
-      message(FATAL_ERROR "No ${PACKAGE_NAME_AND_VERSION} path provided for current platform")
-    endif()
-
-    message(STATUS "Searching ${PACKAGE_NAME_AND_VERSION} on ${ZZPKG_PLATFORM}")
-    message(STATUS "Root directory: ${package_root}")
-
+    message(STATUS "[debug] not a interface library")
+      
     # 常见的 CMake config 文件位置
     set(candidate_subdirs
       ""
@@ -125,39 +149,38 @@ macro(zzpkg_find PACKAGE_NAME_AND_VERSION)
       sdk/native/jni
       sdk/native/jni/abi-${ANDROID_ABI}
       cmake/abi-${ANDROID_ABI}
+      cmake/staticlib
       cmake
     )
 
     # 尝试查找 Config 文件
     set(config_found FALSE)
     foreach(subdir ${candidate_subdirs})
-      set(config_path "${package_root}/${subdir}")
+      set(config_path "${PACKAGE_ROOT}/${subdir}")
       set(config_files
         "${config_path}/${PACKAGE_NAME}Config.cmake"
         "${config_path}/${PACKAGE_NAME}-config.cmake"
       )
       foreach(config_file ${config_files})
+        message(STATUS "[debug] config_file: ${config_file}")
         if(EXISTS "${config_file}")
           set(${PACKAGE_NAME}_DIR "${config_path}")
-          message(STATUS "Found ${PACKAGE_NAME} config in: ${config_path}")
+          message(STATUS "  Found ${PACKAGE_NAME} config file in: ${config_path}")
           set(config_found TRUE)
           break()
         endif()
       endforeach()
       if(config_found)
+        find_package(${PACKAGE_NAME} REQUIRED)
+        message(STATUS "  Found package by using find_package()")
         break()
       endif()
     endforeach()
+  endforeach()
 
-    if(NOT config_found)
-      message(FATAL_ERROR "Could not find ${PACKAGE_NAME}Config.cmake in subdirs of: ${package_root}")
-    endif()
-
-    find_package(${PACKAGE_NAME} REQUIRED)
-  endif()
-
-  message(STATUS "${PACKAGE_NAME}_FOUND: ${${PACKAGE_NAME}_FOUND}")
-  message(STATUS "${PACKAGE_NAME}_VERSION: ${${PACKAGE_NAME}_VERSION}")
+  # if(NOT config_found)
+  #   message(FATAL_ERROR "Could not find ${PACKAGE_NAME}Config.cmake in subdirs of: ${package_root}")
+  # endif()
 
   # 清理临时变量
   unset(ARG_WINDOWS)
@@ -174,7 +197,9 @@ macro(zzpkg_find PACKAGE_NAME_AND_VERSION)
   unset(config_path)
   unset(config_files)
   unset(config_file)
-  unset(_pkg_parts)
+  unset(_recipe_parts)
   unset(PACKAGE_NAME)
   unset(PACKAGE_VERSION)
+  unset(PACKAGE_HINT)
+  unset(PACKAGE_ROOT)
 endmacro()
