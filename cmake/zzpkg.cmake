564de6cd92ce2722174c27a7e99da1bcaffe3238
Zhuo Zhang <imzhuo@foxmail.com>
2026-01-01 13:09:33 +0800

    fix build and import of opencv on windows

--- cmake/zzpkg.cmake
+++ cmake/zzpkg.cmake
@@ -50,6 +50,30 @@ macro(zzpkg_detect_arch)
   endif()
 endmacro()
 
+function(zzpkg_parse_recipe PACKAGE_RECIPE OUT_NAME OUT_VERSION OUT_HINT)
+  # 解析配方
+  string(REPLACE "/" ";" _recipe_parts "${PACKAGE_RECIPE}")
+  string(REPLACE ":" ";" _recipe_parts "${_recipe_parts}")
+  list(LENGTH _recipe_parts _recipe_parts_len)
+  
+  if(_recipe_parts_len LESS 2)
+    message(FATAL_ERROR "PACKAGE_RECIPE must be in format 'PackageName/Version[:hint]'")
+  endif()
+  
+  list(GET _recipe_parts 0 _name)
+  list(GET _recipe_parts 1 _version)
+  set(_hint "")
+  
+  if(_recipe_parts_len EQUAL 3)
+    list(GET _recipe_parts 2 _hint)
+  endif()
+
+  # 关键：使用 ${OUT_NAME} 来引用传入的变量名
+  set(${OUT_NAME} ${_name} PARENT_SCOPE)
+  set(${OUT_VERSION} ${_version} PARENT_SCOPE)
+  set(${OUT_HINT} ${_hint} PARENT_SCOPE)
+endfunction()
+
 # Macro to find a package given platform-specific root paths
 # Usage:
 # zzpkg_find([PACKAGE_NAME/PACKAGE_VERSION] # Package name and version, required
@@ -59,41 +83,32 @@ endmacro()
 #   MACOS    "path/to/macos/package"      # Package install path on macOS, optional
 # )
 macro(zzpkg_find PACKAGE_RECIPE)
-  # 立即解析包名
-  string(REPLACE "/" ";" _recipe_parts "${PACKAGE_RECIPE}")
-  string(REPLACE ":" ";" _recipe_parts "${_recipe_parts}")
-  list(LENGTH _recipe_parts _recipe_parts_len)
-  if(_recipe_parts_len LESS 2)
-    message(FATAL_ERROR "PACKAGE_RECIPE must be in format 'PackageName/Version[:hint]'")
-  endif()
-  
-  list(GET _recipe_parts 0 PACKAGE_NAME)
-  list(GET _recipe_parts 1 PACKAGE_VERSION)
-  set(PACKAGE_HINT)
-  if(_recipe_parts_len EQUAL 3)
-    list(GET _recipe_parts 2 PACKAGE_HINT)
-  endif()
-  
   message(STATUS "Searching package with recipe: ${PACKAGE_RECIPE}")
-  message(STATUS "  PACKAGE_NAME: ${PACKAGE_NAME}")
-  message(STATUS "  PACKAGE_VERSION: ${PACKAGE_VERSION}")
-  message(STATUS "  PACKAGE_HINT: ${PACKAGE_HINT}")
+  zzpkg_parse_recipe(${PACKAGE_RECIPE} pkg_name pkg_version pkg_hint)
+  
+  get_property(is_pkg_imported GLOBAL PROPERTY ${pkg_name}_imported)
+  if(is_pkg_imported)
+    get_property(imported_pkg_version GLOBAL PROPERTY ${pkg_name}_version)
+    if(NOT ${imported_pkg_version} STREQUAL ${pkg_version})
+      message(FATAL_ERROR "conflict version: imported(${imported_pkg_version}) vs to-be-imported(${pkg_version})")
+    endif()
+    get_property(imported_pkg_recipe GLOBAL PROPERTY ${pkg_name}_recipe)
+    message(STATUS "  ${pkg_name} has been imported via recipe ${imported_pkg_recipe}, let's re-use it")
+    set(pkg_recipe ${imported_pkg_recipe})
+  else()
+    set(pkg_recipe ${PACKAGE_RECIPE})
+  endif()
+
+  zzpkg_parse_recipe(${pkg_recipe} pkg_name pkg_version pkg_hint)
+  message(STATUS "  acutal recipe: ${pkg_recipe}")
+  message(STATUS "  pkg_name: ${pkg_name}")
+  message(STATUS "  pkg_version: ${pkg_version}")
+  message(STATUS "  pkg_hint: ${pkg_hint}")
 
   # parse major version
-  string(REPLACE "." ";" _version_parts "${PACKAGE_VERSION}")
-  list(GET _version_parts 0 PACKAGE_VERSION_MAJOR)
-  message(STATUS "  PACKAGE_VERSION_MAJOR: ${PACKAGE_VERSION_MAJOR}")
-
-  # 检查是否已导入
-  get_property(_pkg_imported GLOBAL PROPERTY ${PACKAGE_NAME}_IMPORTED)
-  
-  if(_pkg_imported)
-    get_property(_pkg_version GLOBAL PROPERTY ${PACKAGE_NAME}_VERSION)
-    if(NOT ${_pkg_version} STREQUAL ${PACKAGE_VERSION})
-      message(FATAL_ERROR "conflict version: imported(${_pkg_version}) vs to-be-imported(${PACKAGE_VERSION})")
-    endif()
-    message(STATUS "  Package already imported with same version, continuing to find config...")
-  endif()
+  string(REPLACE "." ";" pkg_version_parts "${pkg_version}")
+  list(GET pkg_version_parts 0 pkg_version_major)
+  message(STATUS "  pkg_version_major: ${pkg_version_major}")
   
   # 解析参数
   cmake_parse_arguments(
@@ -105,90 +120,88 @@ macro(zzpkg_find PACKAGE_RECIPE)
   )
 
   if((ZZPKG_PLATFORM STREQUAL "android") AND ARG_ANDROID)
-    set(CUSTOM_PACKAGE_ROOT "${ARG_ANDROID}")
+    set(custom_pkg_root "${ARG_ANDROID}")
   elseif((ZZPKG_PLATFORM STREQUAL "windows") AND ARG_WINDOWS)
-    set(CUSTOM_PACKAGE_ROOT "${ARG_WINDOWS}")
+    set(custom_pkg_root "${ARG_WINDOWS}")
   elseif((ZZPKG_PLATFORM STREQUAL "mac") AND ARG_MAC)
-    set(CUSTOM_PACKAGE_ROOT "${ARG_MAC}")
+    set(custom_pkg_root "${ARG_MAC}")
   elseif((ZZPKG_PLATFORM STREQUAL "linux") AND ARG_LINUX)
-    set(CUSTOM_PACKAGE_ROOT "${ARG_LINUX}")
+    set(custom_pkg_root "${ARG_LINUX}")
   elseif(ARG_DEFAULT)
-    set(CUSTOM_PACKAGE_ROOT "${ARG_DEFAULT}")
+    set(custom_pkg_root "${ARG_DEFAULT}")
   else()
-    set(CUSTOM_PACKAGE_ROOT)
+    set(custom_pkg_root)
   endif()
 
   # 构建查找路径
-  set(PACKAGE_PLATFORM_INDEPENDENT_ROOT "${ZZPKG_ROOT}/${PACKAGE_NAME}/${PACKAGE_VERSION}")
-  set(PACKAGE_PLATFORM_DEPENDENT_ROOT "${ZZPKG_ROOT}/${PACKAGE_NAME}/${PACKAGE_VERSION}/${ZZPKG_PLATFORM}-${ZZPKG_ARCH}")
-  set(PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT "${PACKAGE_PLATFORM_DEPENDENT_ROOT}")
-  if(PACKAGE_HINT)
-    STRING(APPEND PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT "-${PACKAGE_HINT}")
+  set(pkg_platform_independent_root "${ZZPKG_ROOT}/${pkg_name}/${pkg_version}")
+  set(pkg_platform_dependent_root "${ZZPKG_ROOT}/${pkg_name}/${pkg_version}/${ZZPKG_PLATFORM}-${ZZPKG_ARCH}")
+  set(pkg_platform_dependent_root_with_hint "${pkg_platform_dependent_root}")
+  if(pkg_hint)
+    STRING(APPEND pkg_platform_dependent_root_with_hint "-${pkg_hint}")
   endif()
 
-  message(STATUS "  PACKAGE_PLATFORM_INDEPENDENT_ROOT: ${PACKAGE_PLATFORM_INDEPENDENT_ROOT}")
-  message(STATUS "  PACKAGE_PLATFORM_DEPENDENT_ROOT: ${PACKAGE_PLATFORM_DEPENDENT_ROOT}")
-  message(STATUS "  PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT: ${PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT}")
+  message(STATUS "  pkg_platform_independent_root: ${pkg_platform_independent_root}")
+  message(STATUS "  pkg_PLATFORM_DEPENDENT_ROOT: ${pkg_platform_dependent_root}")
+  message(STATUS "  pkg_platform_dependent_root_with_hint: ${pkg_platform_dependent_root_with_hint}")
 
-  if(NOT EXISTS ${PACKAGE_PLATFORM_INDEPENDENT_ROOT})
-    message(FATAL_ERROR "PACKAGE_PLATFORM_INDEPENDENT_ROOT(${PACKAGE_PLATFORM_INDEPENDENT_ROOT}) does not exist for recipe: ${PACKAGE_RECIPE}")
+  if(NOT EXISTS ${pkg_platform_independent_root})
+    message(FATAL_ERROR "pkg_platform_independent_root(${pkg_platform_independent_root}) does not exist for recipe: ${PACKAGE_RECIPE}")
   endif()
 
-  set(CANDIDATE_PACKAGE_ROOT_LIST)
-  if(CUSTOM_PACKAGE_ROOT)
-    list(APPEND CANDIDATE_PACKAGE_ROOT_LIST ${CUSTOM_PACKAGE_ROOT})
+  set(candidate_pkg_root_list)
+  if(custom_pkg_root)
+    list(APPEND candidate_pk_root_list ${custom_pkg_root})
   endif()
-  if(PACKAGE_HINT AND EXISTS ${PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT})
-    list(APPEND CANDIDATE_PACKAGE_ROOT_LIST ${PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT})
+  if(pkg_hint AND EXISTS ${pkg_platform_dependent_root_with_hint})
+    list(APPEND candidate_pkg_root_list ${pkg_platform_dependent_root_with_hint})
   endif()
-  list(APPEND CANDIDATE_PACKAGE_ROOT_LIST ${PACKAGE_PLATFORM_DEPENDENT_ROOT})
-  list(APPEND CANDIDATE_PACKAGE_ROOT_LIST ${PACKAGE_PLATFORM_INDEPENDENT_ROOT})
+  list(APPEND candidate_pkg_root_list ${pkg_platform_dependent_root})
+  list(APPEND candidate_pkg_root_list ${pkg_platform_independent_root})
 
-  set(PACKAGE_FOUND FALSE)
-  foreach(PACKAGE_ROOT ${CANDIDATE_PACKAGE_ROOT_LIST})
-    if(PACKAGE_FOUND)
+  set(pkg_found FALSE)
+  foreach(pkg_root ${candidate_pkg_root_list})
+    if(pkg_found)
       break()
     endif()
 
-    message(STATUS "  Try find package with PACKAGE_ROOT: ${PACKAGE_ROOT}")
-    if(EXISTS "${PACKAGE_ROOT}/inc")
-      add_library(${PACKAGE_NAME} INTERFACE)
-      target_include_directories(${PACKAGE_NAME} INTERFACE "${PACKAGE_PLATFORM_INDEPENDENT_ROOT}/inc")
-      set_target_properties(${PACKAGE_NAME} PROPERTIES
-        VERSION ${PACKAGE_VERSION}
+    message(STATUS "  try find package with pkg_root: ${pkg_root}")
+    if(EXISTS "${pkg_root}/inc")
+      add_library(${pkg_name} INTERFACE)
+      target_include_directories(${pkg_name} INTERFACE "${pkg_platform_independent_root}/inc")
+      set_target_properties(${pkg_name} PROPERTIES
+        VERSION ${pkg_version}
       )
-      add_library(zzpkg::${PACKAGE_NAME} ALIAS ${PACKAGE_NAME})
-      message(STATUS "  Created INTERFACE target: zzpkg::${PACKAGE_NAME}")
-      set(PACKAGE_FOUND TRUE)
+      add_library(zzpkg::${pkg_name} ALIAS ${pkg_name})
+      message(STATUS "  created INTERFACE target: zzpkg::${pkg_name}")
+      set(pkg_found TRUE)
       break()
     endif()
       
     # 常见的 CMake config 文件位置
     set(candidate_subdirs
       ""
+      cmake/lib # OpenCV Windows, OPENCV_CONFIG_INSTALL_PATH="cmake"
       cmake # OpenCV Linux, OPENCV_CONFIG_INSTALL_PATH="cmake"
       
       # ncnn, glfw
-      lib/cmake/${PACKAGE_NAME}
+      lib/cmake/${pkg_name}
     )
 
-    if(NOT(PACKAGE_NAME MATCHES ".*[0-9]$"))
+    if(NOT(pkg_name MATCHES ".*[0-9]$"))
       list(APPEND candidate_subdirs
         # OpenCV, macOS/Linux, Default; glfw3 macOS, Default
-        lib/cmake/${PACKAGE_NAME}${PACKAGE_VERSION_MAJOR}
+        lib/cmake/${pkg_name}${pkg_version_major}
       )
     endif()
 
     # lib/cmake
     # lib
-    # share/cmake/${PACKAGE_NAME}
-    # share/${PACKAGE_NAME}
+    # share/cmake/${pkg_name}
+    # share/${pkg_name}
 
     if(ZZPKG_OS STREQUAL "windows")
       list(APPEND candidate_subdirs
-        # OpenCV Windows, OPENCV_CONFIG_INSTALL_PATH="cmake"
-        cmake/lib
-
         # OpenCV Windows, BUILD_SHARED_LIBS=ON, Default
         x64/vc18/bin
         x64/vc17/bin
@@ -211,29 +224,29 @@ macro(zzpkg_find PACKAGE_RECIPE)
     endif()
 
     # 尝试查找 Config 文件
-    set(candidate_targets ${PACKAGE_NAME})
-    # 判断 PACKAGE_NAME 最后一个字符是否为数字, 如果不是数字，则添加 major 版本后缀的 target 名称
-    if ((NOT PACKAGE_NAME MATCHES ".*[0-9]$") AND (PACKAGE_VERSION_MAJOR MATCHES "^[0-9]+$"))
-      list(APPEND candidate_targets "${PACKAGE_NAME}${PACKAGE_VERSION_MAJOR}")
+    set(candidate_targets ${pkg_name})
+    # 判断 pkg_name 最后一个字符是否为数字, 如果不是数字，则添加 major 版本后缀的 target 名称
+    if ((NOT pkg_name MATCHES ".*[0-9]$") AND (pkg_version_major MATCHES "^[0-9]+$"))
+      list(APPEND candidate_targets "${pkg_name}${pkg_version_major}")
     endif()
 
-    foreach(targetName ${candidate_targets})
+    foreach(target ${candidate_targets})
       foreach(subdir ${candidate_subdirs})
-        if(PACKAGE_FOUND)
+        if(pkg_found)
           break()
         endif()
-        set(config_path "${PACKAGE_ROOT}/${subdir}")
+        set(config_path "${pkg_root}/${subdir}")
         set(config_files
-          "${config_path}/${targetName}Config.cmake"
-          "${config_path}/${targetName}-config.cmake"
+          "${config_path}/${target}Config.cmake"
+          "${config_path}/${target}-config.cmake"
         )
         foreach(config_file ${config_files})
           # message(STATUS "    Try config file: ${config_file}")
           if(EXISTS "${config_file}")
-            set(${targetName}_DIR "${config_path}")
-            message(STATUS "  find_package(${targetName}) with config file ${config_file}")
-            find_package(${targetName} REQUIRED)
-            set(PACKAGE_FOUND TRUE)
+            set(${target}_DIR "${config_path}")
+            message(STATUS "  find_package(${target}) with config file ${config_file}")
+            find_package(${target} REQUIRED)
+            set(pkg_found TRUE)
             break()
           endif()
         endforeach()
@@ -241,37 +254,36 @@ macro(zzpkg_find PACKAGE_RECIPE)
     endforeach()
   endforeach()
 
-  if(NOT PACKAGE_FOUND)
-    message(FATAL_ERROR "Could not find package with recipe: ${PACKAGE_RECIPE}")
+  if(NOT pkg_found)
+    message(FATAL_ERROR "Could not find ${PACKAGE_RECIPE} with ${pkg_recipe}")
   endif()
   
   # 设置全局导入标记
-  set_property(GLOBAL PROPERTY ${PACKAGE_NAME}_IMPORTED TRUE)
-  set_property(GLOBAL PROPERTY ${PACKAGE_NAME}_VERSION "${PACKAGE_VERSION}")
-  set_property(GLOBAL PROPERTY ${PACKAGE_NAME}_RECIPE "${PACKAGE_RECIPE}")
-  
-  message(STATUS "  Successfully find ${PACKAGE_RECIPE}")
+  set_property(GLOBAL PROPERTY ${pkg_name}_imported TRUE)
+  set_property(GLOBAL PROPERTY ${pkg_name}_version "${pkg_version}")
+  set_property(GLOBAL PROPERTY ${pkg_name}_recipe "${pkg_recipe}")  
+  message(STATUS "  Successfully find ${pkg_recipe}")
   
   # clean up variables
-  unset(_recipe_parts)
-  unset(_recipe_parts_len)
-  unset(PACKAGE_NAME)
-  unset(PACKAGE_VERSION)
-  unset(PACKAGE_HINT)
-  unset(_version_parts)
-  unset(PACKAGE_VERSION_MAJOR)
-  unset(_pkg_imported)
-  unset(_pkg_version)
+  unset(pkg_recipe_parts)
+  unset(pkg_recipe_parts_len)
+  unset(pkg_name)
+  unset(pkg_version)
+  unset(pkg_hint)
+  unset(pkg_version_parts)
+  unset(pkg_version_major)
+  unset(is_pkg_imported)
+  unset(imported_pkg_version)
   unset(ARG_WINDOWS)
   unset(ARG_LINUX)
   unset(ARG_ANDROID)
   unset(ARG_MAC)
-  unset(PACKAGE_PLATFORM_INDEPENDENT_ROOT)
-  unset(PACKAGE_PLATFORM_DEPENDENT_ROOT)
-  unset(PACKAGE_PLATFORM_DEPENDENT_ROOT_WITH_HINT)
-  unset(CANDIDATE_PACKAGE_ROOT_LIST)
-  unset(CUSTOM_PACKAGE_ROOT)
-  unset(PACKAGE_FOUND)
+  unset(package_platform_independent_root)
+  unset(package_platform_dependent_root)
+  unset(package_platform_dependent_root_with_hint)
+  unset(candidate_package_root_list)
+  unset(custom_package_root)
+  unset(package_found)
   unset(candidate_subdirs)
   unset(config_path)
   unset(config_files)
